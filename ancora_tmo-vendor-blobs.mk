3ce1d6e1250b238e3f9f33d029c40459d87fdaf0
jfbs <jeffmbowers@gmail.com>
2012-10-18 21:35:45 -0400

    escape correction

--- ancora_tmo-vendor-blobs.mk
+++ ancora_tmo-vendor-blobs.mk
@@ -39,7 +39,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/ancora_tmo/proprietary//lib/egl/libq3dtools_adreno200.so:system//lib/egl/libq3dtools_adreno200.so \
     vendor/samsung/ancora_tmo/proprietary//lib/hw/lights.msm7k.so:system//lib/hw/lights.ancora_tmo.so \
     vendor/samsung/ancora_tmo/proprietary//lib/hw/sensors.SGH-T679.so:system//lib/hw/sensors.ancora_tmo.so \
-    vendor/samsung/ancora_tmo/proprietary//lib/hw/camera.msm7x30.so:system//lib/hw/camera.msm7x30.so
+    vendor/samsung/ancora_tmo/proprietary//lib/hw/camera.msm7x30.so:system//lib/hw/camera.msm7x30.so \
     vendor/samsung/ancora_tmo/proprietary//lib/libActionShot.so:system//lib/libActionShot.so \
     vendor/samsung/ancora_tmo/proprietary//lib/libakm.so:system//lib/libakm.so \
     vendor/samsung/ancora_tmo/proprietary//lib/libarccamera.so:system//lib/libarccamera.so \
