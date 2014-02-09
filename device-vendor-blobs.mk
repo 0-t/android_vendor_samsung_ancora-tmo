1074c6c9435a553c5f57e029e0314ef10b0ff097
jfbs <jeffmbowers@gmail.com>
2014-02-09 15:53:51 -0500

    Revert "use opensource libril, picked from: czobor@9231e02704864b2c27d4fcd3903dba44e8db4edf"
    This reverts commit a6da0bc31dc681beeda35f03db13accc971b44ab.

--- device-vendor-blobs.mk
+++ device-vendor-blobs.mk
@@ -18,6 +18,7 @@
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
     vendor/samsung/ancora_tmo/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
+    vendor/samsung/ancora_tmo/proprietary/lib/libril.so:obj/lib/libril.so \
     vendor/samsung/ancora_tmo/proprietary/lib/libseccameraadaptor.so:obj/lib/libseccameraadaptor.so \
     vendor/samsung/ancora_tmo/proprietary/lib/libsecril-client.so:obj/lib/libsecril-client.so
 
@@ -97,5 +98,7 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_COPY_FILES += \
+    vendor/samsung/ancora_tmo/proprietary/bin/rild:system/bin/rild \
+    vendor/samsung/ancora_tmo/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/samsung/ancora_tmo/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/ancora_tmo/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so
