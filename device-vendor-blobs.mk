arco/android_vendor_samsung_ancora

add radish to enable IPv6 tethering

device-vendor-blobs.mk
@@ -21,7 +21,8 @@ PRODUCT_COPY_FILES += \
 
 # Binaries
 PRODUCT_COPY_FILES += \
-    vendor/samsung/ancora/proprietary/bin/rmt_storage:system/bin/rmt_storage
+    vendor/samsung/ancora/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+    vendor/samsung/ancora/proprietary/bin/radish:/system/bin/radish
 
 # Adreno libs
 PRODUCT_COPY_FILES += \

0-t/android_vendor_samsung_ancora-tmo

Conflicts:
	device-vendor-blobs.mk

device-vendor-blobs.mk
@@ -22,7 +22,8 @@ PRODUCT_COPY_FILES += \
 
 # Binaries
 PRODUCT_COPY_FILES += \
-    vendor/samsung/ancora_tmo/proprietary/bin/rmt_storage:system/bin/rmt_storage
+    vendor/samsung/ancora_tmo/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+    vendor/samsung/ancora_tmo/proprietary/bin/radish:system/bin/radish
 
 # Adreno libs
 PRODUCT_COPY_FILES += \