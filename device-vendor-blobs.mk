arco/android_vendor_samsung_ancora

Fix compilation after rild updates
* Don't really think we need to copy these to obj anymore.
  Can't remember why we did it in the first place anyway.

device-vendor-blobs.mk
@@ -17,9 +17,7 @@
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
     vendor/samsung/ancora/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
-    vendor/samsung/ancora/proprietary/lib/libril.so:obj/lib/libril.so \
-    vendor/samsung/ancora/proprietary/lib/libseccameraadaptor.so:obj/lib/libseccameraadaptor.so \
-    vendor/samsung/ancora/proprietary/lib/libsecril-client.so:obj/lib/libsecril-client.so
+    vendor/samsung/ancora/proprietary/lib/libseccameraadaptor.so:obj/lib/libseccameraadaptor.so
 
 # Binaries
 PRODUCT_COPY_FILES += \

0-t/android_vendor_samsung_ancora-tmo

Conflicts:
	device-vendor-blobs.mk

device-vendor-blobs.mk
@@ -18,9 +18,7 @@
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
     vendor/samsung/ancora_tmo/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
-    vendor/samsung/ancora_tmo/proprietary/lib/libril.so:obj/lib/libril.so \
-    vendor/samsung/ancora_tmo/proprietary/lib/libseccameraadaptor.so:obj/lib/libseccameraadaptor.so \
-    vendor/samsung/ancora_tmo/proprietary/lib/libsecril-client.so:obj/lib/libsecril-client.so
+    vendor/samsung/ancora_tmo/proprietary/lib/libseccameraadaptor.so:obj/lib/libseccameraadaptor.so
 
 # Binaries
 PRODUCT_COPY_FILES += \